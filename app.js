cfe6ee5c584e3a82b3b3fef285823897a584560a
victor toledo <querynetworking@gmail.com>
2023-05-04 00:01:27 -0300

    webserver-expess-hbs-demo

--- app.js
+++ app.js
@@ -1,8 +1,44 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const hbs = require('hbs');
+require('dotenv').config();
 
-app.get('/', function (req, res) {
-  res.send('Hello World')
-})
+const app = express();
+const port = process.env.PORT;
 
-app.listen(8080)
+// Handlebars
+app.set('view engine', 'hbs');
+hbs.registerPartials(__dirname + '/views/partials');
+
+// Servir contenido estático
+app.use(express.static('public'));
+
+
+app.get('/', (req, res) => {
+    res.render('home', {
+        nombre: 'Victor Toledo',
+        titulo: 'Sitio Oficial'
+    });
+});
+
+app.get('/generic', (req, res) => {
+    res.render('generic', {
+        nombre: 'Victor Toledo',
+        titulo: 'Sitio Oficial'
+    });
+});
+
+app.get('/elements', (req, res) => {
+    res.render('elements', {
+        nombre: 'Victor Toledo',
+        titulo: 'Sitio Oficial'
+    });
+});
+
+
+app.get('*', (req, res) => {
+    res.sendFile(__dirname + '/public/404.html')
+});
+
+app.listen(port, () => {
+    console.log(`Escuchando peticiones en el puerto ${ port }`);
+});
